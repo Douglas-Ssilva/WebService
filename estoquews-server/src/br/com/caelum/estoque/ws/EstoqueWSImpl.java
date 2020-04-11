e0281802fffb0f1e9502a00d153254ea6b222295
Douglas Silva <douglas_d1994@hotmail.com>
2020-04-11 13:19:37 -0300

    Commit

---
+++ estoquews-server/src/br/com/caelum/estoque/ws/EstoqueWSImpl.java
@@ -0,0 +1,37 @@
+package br.com.caelum.estoque.ws;
+
+import java.util.Arrays;
+
+import javax.jws.WebService;
+
+//Class feita pela equipe de desenvolvimento
+@WebService(endpointInterface = "br.com.caelum.estoque.ws.EstoqueWS", serviceName = "estoqueWS", portName = "EstoqueWSPort")
+public class EstoqueWSImpl implements EstoqueWS {
+	@Override
+	public ListaItens todosOsItens(Filtros filtros) {
+		System.out.println("Chamando todos os Itens");
+		ListaItens listaItens = new ListaItens();
+		listaItens.item = Arrays.asList(geraItem());
+		return listaItens;
+	}
+
+	@Override
+	public CadastrarItemResponse cadastrarItem(CadastrarItem parameters, TokenUsuario tokenUsuario)
+			throws AutorizacaoFault {
+		System.out.println("Chamando cadastarItem");
+		CadastrarItemResponse resposta = new CadastrarItemResponse();
+		resposta.setItem(geraItem());
+		return resposta;
+	}
+
+	// método auxiliar
+	private Item geraItem() {
+		Item item = new Item();
+		item.codigo = "MEA";
+		item.nome = "MEAN";
+		item.quantidade = 5;
+		item.tipo = "Livro";
+		return item;
+	}
+
+}
